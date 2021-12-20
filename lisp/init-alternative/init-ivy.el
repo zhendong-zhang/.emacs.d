d5349655246733c517a814a224fad6e808d46270
zhendong <zhendong.zhang.zh@gmail.com>
2021-12-20 22:16:14 +0800

    类find-file命令默认选中当前文件

--- lisp/init-alternative/init-ivy.el
+++ lisp/init-alternative/init-ivy.el
@@ -29,7 +29,12 @@
    ("f" . forward-char)
    ("x" . ivy-occur-press))
   :config
-  (ivy-mode))
+  (ivy-mode)
+
+  ;; FIXME: 解决find-file、load-file未默认选中当前文件，但会导致补全列表里当前文件有两个
+  ;; 传入`ivy-read'的def值有问题，应置为nil，可考虑如`counsel-find-file'另加接口
+  (setq insert-default-directory nil)
+  )
 
 (use-package imenu-anywhere
   :bind
@@ -51,14 +56,18 @@
 
 (use-package counsel
   :demand
+  :init
+  (setq counsel-preselect-current-file t)
   :bind
   (([remap execute-extended-command] . counsel-M-x)
+   ([remap find-file] . counsel-find-file)
    ("M-g l" . counsel-locate)
    ("M-g M" . counsel-bookmark)
    ("M-g m" . counsel-mark-ring)
    ("M-s a" . counsel-ag)
    ("M-s r" . counsel-rg)
-   ("M-g o" . counsel-rg)))
+   ("M-g o" . counsel-rg)
+   ("M-g F" . counsel-file-jump)))
 
 (use-package counsel-projectile
   :after (counsel projectile)
