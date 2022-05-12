33d926914f299fd487f00557ec1512da342dc480
zhendong <zhendong.zhang.zh@gmail.com>
2022-05-12 13:44:02 +0800

    定时更新

---
+++ lisp/init-alternative/init-auto-package-update.el
@@ -0,0 +1,7 @@
+(use-package auto-package-update
+  :config
+  (setq auto-package-update-delete-old-versions t)
+  (setq auto-package-update-hide-results t)
+  (auto-package-update-maybe))
+
+(provide 'init-auto-package-update)
