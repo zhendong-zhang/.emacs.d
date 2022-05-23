12f9d3d8b0fe44bbea1a2d44fec64b7e1a8d4a9d
zhendong <zhendong.zhang.zh@gmail.com>
2022-05-23 17:16:04 +0800

    设置custom-file

--- init.el
+++ init.el
@@ -87,7 +87,10 @@
   (require-dir "init-modes")
   ;; 支持emacs client
   (require 'init-daemon)
+
+  (setq custom-file (expand-file-name ".emacs-custom.el" user-emacs-directory))
+  (when (file-exists-p custom-file)
+    (load custom-file))
 )
 
 (provide 'init)
-
