d061805221ba21fe6a97049b6d025f073da8172b
zhendong <zhendong.zhang.zh@gmail.com>
2022-07-08 11:05:24 +0800

    support emacsclient better

--- lisp/init-daemon.el
+++ lisp/init-daemon.el
@@ -1,7 +1,5 @@
 (use-package server
   :functions (server-running-p)
-  :init
-  (setq server-auth-dir (no-littering-expand-var-file-name "server/"))
   :config
   (unless (or (daemonp) (server-running-p))
   (server-start)))
