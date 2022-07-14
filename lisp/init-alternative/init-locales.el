16ab95781f8f11e4551f7b2bbe686767bc4241e3
zhendong <zhendong.zhang.zh@gmail.com>
2022-07-14 15:42:01 +0800

    处理中文乱码问题

--- lisp/init-alternative/init-locales.el
+++ lisp/init-alternative/init-locales.el
@@ -10,9 +10,21 @@
 (prefer-coding-system 'utf-16)
 (prefer-coding-system 'utf-8)
 
-;; 解决windows emacs调用外部进程的中文乱码问题
 (when is-windows-nt
-  (w32-set-system-coding-system 'gbk)
-  (add-to-list 'process-coding-system-alist '("[cC][mM][dD][pP][rR][oO][xX][yY]" . (utf-8 . utf-8))))
+  ;; 解决windows emacs调用外部进程的中文乱码问题
+  (defadvice projectile-files-via-ext-command (around my-projectile-files-via-ext-command activate)
+    (let ((cmdproxy-old-encoding (cdr (assoc "[cC][mM][dD][pP][rR][oO][xX][yY]" process-coding-system-alist))))
+      (modify-coding-system-alist 'process "[cC][mM][dD][pP][rR][oO][xX][yY]" '(utf-8 . utf-8))
+      ad-do-it
+      (modify-coding-system-alist 'process "[cC][mM][dD][pP][rR][oO][xX][yY]" cmdproxy-old-encoding)
+      ))
+  (add-to-list 'process-coding-system-alist '("[eE][sS]" . (gbk . gbk)))
+  (add-to-list 'process-coding-system-alist '("[gG][iI][tT]" . (utf-8 . utf-8)))
+  ;; emacs client 中文文件名乱码问题
+  (setq w32-unicode-filenames nil)
+  (setq file-name-coding-system 'gb18030)
+  ;(w32-set-system-coding-system 'gb18030)
+  ;(set-language-environment 'gb18030)
+  )
 
 (provide 'init-locales)
