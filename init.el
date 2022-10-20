541eb47654e2488b27b30e6de20f12cc475de10a
zhendong <zhendong.zhang.zh@gmail.com>
2022-10-20 16:34:56 +0800

    调整缩进

--- init.el
+++ init.el
@@ -64,12 +64,12 @@
       (error "no one or more than one dir match, do nothing for require-dir %s." dir))))
 
 (if (and (fboundp 'native-comp-available-p)
-       (native-comp-available-p))
-  (message "Native compilation is available")
-(message "Native complation is *not* available"))
+         (native-comp-available-p))
+    (message "Native compilation is available")
+  (message "Native complation is *not* available"))
 (if (functionp 'json-serialize)
-  (message "Native JSON is available")
-(message "Native JSON is *not* available"))
+    (message "Native JSON is available")
+  (message "Native JSON is *not* available"))
 
 ;; 配置
 
