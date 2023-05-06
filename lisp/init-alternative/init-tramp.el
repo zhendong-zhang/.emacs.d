e6c4f07a5491736ec184f91526c623c84a4f1fef
zhendong <zhendong.zhang.zh@gmail.com>
2023-05-06 16:41:23 +0800

    plink设置

--- lisp/init-alternative/init-tramp.el
+++ lisp/init-alternative/init-tramp.el
@@ -2,6 +2,6 @@
   :defer t
   :init
   (setq tramp-auto-save-directory "~/tmp/tramp/")
-  (setq tramp-default-method "ssh"))
+  (setq tramp-default-method (if is-windows-nt "plink" "ssh")))
 
 (provide 'init-tramp)
