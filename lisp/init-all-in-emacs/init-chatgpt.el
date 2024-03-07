98d1962ffffdd3c5a502987e27045cc79fae22b9
zhendong <zhendong.zhang.zh@gmail.com>
2024-03-07 23:27:36 +0800

    remove mind-wave.

--- lisp/init-all-in-emacs/init-chatgpt.el
+++
@@ -1,8 +0,0 @@
-;; 需设置环境变量OPENAI_API_KEY
-(use-package mind-wave
-  :github "manateelazycat/mind-wave"
-  :mode ("\\.chat$" . mind-wave-chat-mode)
-  :config
-  (setq mind-wave-auto-change-title nil))
-
-(provide 'init-chatgpt)
