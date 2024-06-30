778a9be83bdc75675586d6cad90e3623aa777eaa
zhendong <zhendong.zhang.zh@gmail.com>
2024-06-30 16:20:23 +0800

    分别配置http代理和socks代理的端口号

--- lisp/init-all-in-emacs/init-proxy.el
+++ lisp/init-all-in-emacs/init-proxy.el
@@ -1,21 +1,25 @@
 (defvar my-proxy-ip "127.0.0.1")
-(defvar my-proxy-port "1080")
-(defvar my-proxy (format "%s:%s" my-proxy-ip my-proxy-port))
+(defvar my-http-proxy-port "1080")
+(defvar my-http-proxy (format "%s:%s" my-proxy-ip my-http-proxy-port))
+(defvar my-socks-proxy-port "1081")
+(defvar my-socks-proxy (format "%s:%s" my-proxy-ip my-socks-proxy-port))
 
 ;; Network Proxy
 (defun proxy-http-show ()
   "Show HTTP/HTTPS proxy."
   (interactive)
   (if url-proxy-services
-      (message "Current HTTP proxy is `%s'" my-proxy)
+      (message "Current HTTP proxy is `%s'" my-http-proxy)
     (message "No HTTP proxy")))
 
 (defun proxy-http-enable ()
   "Enable HTTP/HTTPS proxy."
   (interactive)
+  (proxy-socks-disable)
   (setq url-proxy-services
-        `(("http" . ,my-proxy)
-          ("https" . ,my-proxy)
+        `(("http" . ,my-http-proxy)
+          ("https" . ,my-http-proxy)
+          ("ftp" . ,my-http-proxy)
           ("no_proxy" . "^\\(localhost\\|127.0.0.1\\|::1\\|192.168.*\\|10.*\\)")))
   (proxy-http-show))
 
@@ -43,11 +47,12 @@
 (defun proxy-socks-enable ()
   "Enable SOCKS proxy."
   (interactive)
+  (proxy-http-disable)
   (require 'socks)
   (setq url-gateway-method 'socks
         socks-noproxy '("localhost"))
-  (setq socks-server `("Default server" ,my-proxy-ip ,(string-to-number my-proxy-port) 5))
-  (setenv "all_proxy" (concat "socks5://" my-proxy))
+  (setq socks-server `("Default server" ,my-proxy-ip ,(string-to-number my-socks-proxy-port) 5))
+  (setenv "all_proxy" (concat "socks5://" my-socks-proxy))
   (proxy-socks-show))
 
 (defun proxy-socks-disable ()
@@ -77,8 +82,17 @@
 
 (defun proxy-try-enable-proxy ()
   (interactive)
-  (make-network-process :name "proxy" :host my-proxy-ip :service my-proxy-port
+  (make-network-process :name "proxy" :host my-proxy-ip :service my-socks-proxy-port
                         :nowait t :sentinel #'proxy-on-process-result))
 (proxy-try-enable-proxy)
+
+;; for debug
+;; (display-buffer
+;;  (url-retrieve-synchronously
+;;   "https://www.github.com"))
+;; (display-buffer
+;;  (url-retrieve-synchronously
+;;   "http://www.github.com"))
+
 
 (provide 'init-proxy)
