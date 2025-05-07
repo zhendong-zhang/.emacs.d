c7ffffdb11128077b70372574f4b4510c3aada2e
zhendong <zhendong.zhang.zh@gmail.com>
2025-05-07 22:59:12 +0800

    延长midnight-mode下自动清除标签页中buffer的时间到30天

--- lisp/init-looks/init-tab-bar.el
+++ lisp/init-looks/init-tab-bar.el
@@ -1,5 +1,9 @@
 (use-package tab-bar
   :ensure nil
+  :preface
+  (defun my-clean-buffer-list-delay (oldfun name)
+    (or (and (tab-bar-get-buffer-tab name) (* 30 24 60 60))
+        (apply oldfun (list name))))
   :custom
   (tab-bar-format '(tab-bar-format-tabs tab-bar-separator))
   (tab-bar-close-button-show nil)
@@ -12,20 +16,21 @@
   (tab-bar-mode 1)
   (custom-set-faces
    '(tab-bar
-     ((t (:inherit mode-line-inactive :foreground nil :box nil))))
+     ((t (:inherit mode-line-inactive :foreground unspecified :box nil))))
    '(tab-bar-tab
      ((default
-       :inherit mode-line-inactive :box nil :background nil)
+       :inherit mode-line-inactive :box nil :background unspecified)
       (((background dark))
        :foreground "cyan4")
       (t
        :foreground "red4")))
    '(tab-bar-tab-inactive
      ((default
-       :inherit mode-line-inactive :box nil :background nil)
+       :inherit mode-line-inactive :box nil :background unspecified)
       (((background dark))
        :foreground "white")
       (t
-       :foreground "black")))))
+       :foreground "black"))))
+  (advice-add 'clean-buffer-list-delay :around 'my-clean-buffer-list-delay))
 
 (provide 'init-tab-bar)
