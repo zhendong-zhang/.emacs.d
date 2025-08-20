c7fac8cea0f3da763192e3e92eabe46bf75c8195
zhendong <zhendong.zhang.zh@gmail.com>
2025-08-20 15:11:04 +0800

    emms：调整配置并去除警告。

--- lisp/init-all-in-emacs/init-music-player.el
+++ lisp/init-all-in-emacs/init-music-player.el
@@ -1,10 +1,17 @@
-(provide 'init-music-player)
+;;; init-music-player.el --- 音乐播放器 -*- lexical-binding: t -*-
+
+;; Author: (zhendong zhendong.zhang.zh@gmail.com)
+
+;;; Code:
 
 (use-package emms
   :hook ((emms-player-started . emms-show)
          (kill-emacs . my-emms-history-save))
-  :commands emms emms-history-load
-  :defines emms-tag-editor-rename-format emms-playlist-mode-map
+  :commands emms emms-history-load emms-history-save emms-stop
+  :bind (:map emms-playlist-mode-map
+              ("D" . emms-delete-file-from-disk))
+  :defines emms-playlist-mode-map
+  :functions emms-track-get emms-playlist-track-at emms-playlist-selected-track-at-p emms-playlist-mode-kill-entire-track emms-all emms-track-simple-description
   :custom
   (emms-lyrics-scroll-p nil)
   (emms-info-functions '(emms-info-native))
@@ -15,6 +22,8 @@
   (defun my-emms-history-save ()
     (when (featurep 'emms)
       (emms-history-save)))
+
+  :config
   (defun emms-delete-file-from-disk ()
     "Delete this file from disk."
     (interactive)
@@ -26,16 +35,30 @@
                     (emms-playlist-selected-track-at-p))
            (emms-stop))
          (emms-playlist-mode-kill-entire-track)
-         (dired-delete-file file)
+         (delete-file file)
          (message (format "Have delete \' %s \' from disk." file))))))
 
-  :bind (:map emms-playlist-mode-map
-         ("D" . emms-delete-file-from-disk))
-  :config
+  (defun emms-info-track-simple-description (track)
+    "Return a simple description of TRACK."
+    (let ((artist    (emms-track-get track 'info-artist))
+          (title     (emms-track-get track 'info-title)))
+      (cond
+       ((and artist title)
+        (concat artist " - " title))
+       (title
+        title)
+       (t
+        (emms-track-simple-description track)))))
+
   (require 'emms-setup)
   (push "aac" emms-player-base-format-list)
   (put 'emms-browser-delete-files 'disabled nil)
   (setq-default emms-tag-editor-rename-format "%t - %a")
   (emms-all)
+  (setq emms-track-description-function 'emms-info-track-simple-description)
+  (setq emms-playlist-default-major-mode 'emms-mark-mode)
   (emms-history-load))
 
+(provide 'init-music-player)
+
+;;; init-music-player.el ends here
